baefaea0e7e8033b10f934f092a2cfe594ce6b7f
Nouali Sarah <msrnouali@live.fr>
2020-04-17 01:06:21 +0200

    feat_addPatient

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const electron = require("electron");
 const path = require("path");
 const url = require("url");
-
+const Window = require('./Window')
 // SET ENV
 process.env.NODE_ENV = "development";
 
@@ -10,10 +10,16 @@ const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addRdvWindow;
+let addPatientWin;
+let addTodoWin
 // Listen for app to be ready
 app.on("ready", function () {
   // Create new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+            nodeIntegration: true
+        }
+  });
   // Load html in window
   mainWindow.loadURL(
     url.format({
@@ -31,6 +37,7 @@ app.on("ready", function () {
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
   Menu.setApplicationMenu(mainMenu);
+
 });
 
 // Handle add item window
@@ -55,7 +62,76 @@ ipcMain.on("ajouter-rdv", () => {
     addRdvWindow = null;
   });
 });
-const mainMenuTemplate = [];
+
+
+
+ipcMain.on('add-patient-window', () => {
+  // if addTodoWin does not already exist
+  if (!addPatientWin) {
+    // create a new add patient window
+    addPatientWin = new Window({
+      file: path.join(__dirname, "renderer", 'addPatient.html'),
+      width: 500,
+      height: 350,
+      title:'Ajouter un patient',
+      // close with the main window
+      parent: mainWindow
+    })
+
+    // cleanup
+    addPatientWin.on('closed', () => {
+      addPatientWin = null
+    })
+  }
+})
+function createAddPatientWindow(){
+  addPatientWindow = new BrowserWindow({
+    width: 500,
+    height:350,
+    title:'Ajouter un patient'
+  });
+  addPatientWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "renderer", 'addPatient.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Handle garbage collection
+  addPatientWindow.on('close', function(){
+    addPatientWindow = null;
+  });
+}
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+  {
+    label: 'File',
+    submenu:[
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Patient',
+    submenu:[
+      {
+        label:'Ajouter Patient',
+        click(){
+          createAddPatientWindow();
+        }
+      },
+      {
+        label:'Supprimer Patient',
+      },
+      {
+        label: 'Afficher la liste des patients',
+      }
+    ]
+  }
+];
 // Add developer tools option if in dev
 if (process.env.NODE_ENV !== "production") {
   mainMenuTemplate.push({
--- renderer/index.js
+++ renderer/index.js
@@ -4,3 +4,7 @@ const { ipcRenderer } = require("electron");
 document.getElementById("ajouter-rdv-btn").addEventListener("click", () => {
   ipcRenderer.send("ajouter-rdv");
 });
+// create add todo window button
+document.getElementById('createPatientBtn').addEventListener('click', () => {
+  ipcRenderer.send('add-patient-window')
+})
