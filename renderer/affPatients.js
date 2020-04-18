a07ce908302a58ae049180961958ff4b0e6c1033
Mihoubi Lamia Zohra <gl_mihoubi@esi.dz>
2020-04-18 09:34:14 +0200

    fixed patients display bug

--- renderer/affPatients.js
+++ renderer/affPatients.js
@@ -1,27 +1,25 @@
-const electron = require('electron');
-const {ipcRenderer} = electron;
-const ul = document.querySelector('ul');
-const DataStore = require('/home/nsarah/Documents/2CS-SIQ3-S2/2019-2020/ALOG/TP/TP1/MedRDV/DataStore')
+const electron = require("electron");
+const { ipcRenderer } = electron;
+const ul = document.querySelector("ul");
+const DataStore = require("../DataStore");
 // Catch item:aff
-const patientsData = new DataStore({ name: 'Patients Main' });
+const patientsData = new DataStore({ name: "Patients Main" });
 let affPatientsWindow;
 console.log("holaaaaaaaa");
 const patientItems = patientsData.getPatient();
-const tt=JSON.stringify(patientItems.get())
+const tt = JSON.stringify(patientItems.get());
 
-const pat =patientItems.get()
-const item=['hello','good'];
-ul.className = 'collection';
+const pat = patientItems.get();
+const item = ["hello", "good"];
+ul.className = "collection";
 
-for (let key in pat){
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-  li.appendChild(document.createTextNode(key+"\n"+String(patientItems.get(key))));
+for (let key in pat) {
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(
+    document.createTextNode(key + "\n" + String(patientItems.get(key)))
+  );
   ul.appendChild(li);
-  
 }
 
 //const itemText = document.createTextNode(JSON.stringify(pat[0]));
-
-
-
